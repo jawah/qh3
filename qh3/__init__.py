2cf68eed722ed06e7a5df21e8862ceef40102197
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-01-15 08:58:14 +0100

    :bookmark: bump version to 1.3.1

--- qh3/__init__.py
+++ qh3/__init__.py
@@ -13,7 +13,7 @@ from .quic.logger import QuicFileLogger, QuicLogger
 from .quic.packet import QuicProtocolVersion
 from .tls import CipherSuite, SessionTicket
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 __all__ = (
     "connect",
